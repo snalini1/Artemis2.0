e388bd9ee3ffb97e5452ae92b59d539a7fd2418d
snalini1 <161677034+snalini1@users.noreply.github.com>
2025-02-09 08:55:32 -0500

    Update safetydata.py

--- Backend/safetydata.py
+++ Backend/safetydata.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Configure CORS to allow requests from the frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Update this for production
+    allow_origins=["http://localhost:3000"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
